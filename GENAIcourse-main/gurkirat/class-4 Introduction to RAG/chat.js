3cb47319594a5f46109383e472ffe4a6fcb6dd7e
Gurkirat <gurkirat.singh@theknowledgeacademy.com>
2025-10-03 14:07:24 +0530

    rag

--- GENAIcourse-main/gurkirat/class-4 Introduction to RAG/chat.js
+++ GENAIcourse-main/gurkirat/class-4 Introduction to RAG/chat.js
@@ -6,9 +6,9 @@ import OpenAI from 'openai';
 const client = new OpenAI();
 
 async function chat() {
-    // const userQuery = 'Tell me about adding two number in python';
-    // const userQuery = 'Tell me about debugging in nodejs and give me source also';
-    const userQuery = 'Tell me about debugging in nodejs and give me some examples and source also';
+    const userQuery = 'Tell me about adding two number in python';
+    // const userQuery = 'Tell me about components in react and also the source';
+    // const userQuery = 'Tell me about debugging in nodejs and give me some examples and source also';
 
     // Ready the client OpenAI Embedding Model
     const embeddings = new OpenAIEmbeddings({
@@ -30,17 +30,16 @@ async function chat() {
     const relevantChunk = await vectorSearcher.invoke(userQuery);
 
     const SYSTEM_PROMPT = `
-    You are an AI assistant who helps resolving user query based on the
-    context available to you from a PDF file with the content and page number.
+        You are an AI assistant who helps resolving user query based on the context available to you from a PDF file with the content and page number.
 
-    Only ans based on the available context from file only.
+        Only ans based on the available context from file only.
 
-    Context:
-    ${JSON.stringify(relevantChunk)}
-  `;
+        Context: 
+        ${JSON.stringify(relevantChunk)}
+    `;
 
     const response = await client.chat.completions.create({
-        model: 'gpt-4',
+        model: 'gpt-4.1-mini',
         messages: [
             { role: 'system', content: SYSTEM_PROMPT },
             { role: 'user', content: userQuery },
--- GENAIcourse-main/gurkirat/class-4 Introduction to RAG/indexing.js
+++ GENAIcourse-main/gurkirat/class-4 Introduction to RAG/indexing.js
@@ -1,22 +1,33 @@
-// https://github.com/piyushgarg-dev/genai-js-1.0/tree/chore/model-changes
-import 'dotenv/config';
-import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import 'dotenv/config'
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { OpenAIEmbeddings } from '@langchain/openai';
 import { QdrantVectorStore } from '@langchain/qdrant';
 
 async function init() {
-    const pdfFilePath = "./nodejs.pdf";
-    const loader = new PDFLoader(pdfFilePath);
+    const filePath = "./react-beginners-handbook.pdf";
+    const loader = new PDFLoader(filePath);
 
-    // Page by page load the PDF file
     const docs = await loader.load();
 
-    // Ready the client OpenAI Embedding Model
+    // if you want to do according to chunk size then do this else directly do embedding
+    const chunkSize = 1000;
+    const chunkedDocs = docs.flatMap(doc => {
+        const chunks = [];
+        let docText = doc.pageContent;
+        for (let i = 0; i < docText.length; i += chunkSize) {
+            chunks.push({
+                pageContent: docText.substring(i, i + chunkSize),
+                metadata: doc.metadata,
+            });
+        }
+        return chunks;
+    });
+
     const embeddings = new OpenAIEmbeddings({
         model: 'text-embedding-3-large',
     });
 
-    const vectorStore = await QdrantVectorStore.fromDocuments(docs, embeddings, {
+    const vectorStore = await QdrantVectorStore.fromDocuments(chunkedDocs, embeddings, {
         url: 'http://localhost:6333',
         collectionName: 'chaicode-collection',
     });
