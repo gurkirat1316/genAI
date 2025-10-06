c5d3b664b327aa945d5ef50316b9746179b84911
Gurkirat <gurkirat.singh@theknowledgeacademy.com>
2025-10-06 13:51:52 +0530

    nextjs

---
+++ GENAIcourse-main/gurkirat/class-4 Introduction to RAG/chaicode-ui/src/app/contact-us/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function ContactUs() {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <p>This is the Contact Us page.</p>
+            <Link href="/">Go back Home</Link>
+        </div>
+    );
+}
---
+++ GENAIcourse-main/gurkirat/class-4 Introduction to RAG/chaicode-ui/src/app/privacy-policy/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function PrivacyPolicy() {
+    return (
+        <div>
+            <h1>Privacy Policy</h1>
+            <p>This is the Privacy Policy page.</p>
+            <Link href="/">Go back Home</Link>
+        </div>
+    );
+}
