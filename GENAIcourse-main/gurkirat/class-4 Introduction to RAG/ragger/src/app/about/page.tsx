6b9496b6ce487ab39580958252616a440a6b7c78
Gurkirat <gurkirat.singh@theknowledgeacademy.com>
2025-10-21 15:43:48 +0530

    changes in pages for the routing purpose

--- GENAIcourse-main/gurkirat/class-4 Introduction to RAG/ragger/src/app/about/page.tsx
+++ GENAIcourse-main/gurkirat/class-4 Introduction to RAG/ragger/src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import AboutInfo from "@/components/About";
+import Navbar from "@/components/Navbar";
 
 export default function About() {
     return (
         <>
             <div>
+                <Navbar />
                 <AboutInfo />
             </div>
         </>
--- GENAIcourse-main/gurkirat/class-4 Introduction to RAG/ragger/src/components/About.tsx
+++ GENAIcourse-main/gurkirat/class-4 Introduction to RAG/ragger/src/components/About.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import React from 'react';
 import { CardHoverEffectDemo } from './AboutCard';
 import Foot from './Foot';
